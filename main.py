290d0911a5856766d4764b0bb90502935b767b4f
SenierLink <1325181337@qq.com>
2019-06-03 22:14:01 +0800

    完成了读出目录开发，下一步进行数据对比 在这里发现，readlines方法如果和write方法同时使用，读取的文件夹需要关闭，然后再开启一次。

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 # -*- coding: UTF-8 -*-
 import os
-sourse_file = [[],[],[]] #储存需要备份的文件内容
-back_file =[[],[],[]]    #储存已备份文件夹中的内容
+
 copy_file =[[],[],[]]
 store_file = 'E:'
 old_content_path = store_file + "old.txt"
 new_content_path = store_file + "new.txt"
-old_content_file = open(old_content_path, 'a')
-new_content_file = open(new_content_path, 'a')
-i = 0
+copy_content_path = store_file + "copy.txt"
+old_content_file = open(old_content_path, 'w')  #储存需要备份的文件内容
+new_content_file = open(new_content_path, 'w')  #储存已备份文件夹中的内容
+copy_content_file = open(copy_content_path, 'w')        #储存需要复制的内容
 
-print ('hello world')
-path = 'E:\\Honor数据备份\\Pictures\\iphone'
+old_path = 'E:\\Honor数据备份\\Pictures\\iphone'
+new_path = 'E:\\Honor数据备份\\Pictures'
 
 
-def readFileList(myList,path):
+def readFileList(path,filename):
     """
         读取文件列表，并且创造一个三维列表，内涵目录下所有文件名字
         Args:
@@ -22,6 +22,27 @@ def readFileList(myList,path):
             path:需要读取的目录
     """
     for (root,dirs,files) in os.walk(path):
-        # old_content_file.wirte(str(files))
-        old_content_file.wirte('a')
-readFileList(sourse_file,path)
+        for every_root in root:    
+            for every_files in files:
+                filename.write(str(root)+'\\'+str(every_files)+'\n')      
+    print('完成文件写入')
+readFileList(old_path,old_content_file)
+readFileList(new_path,new_content_file)
+
+
+old_content_file.close()
+new_content_file.close()
+
+old_content_file = open(old_content_path, 'r')  #储存需要备份的文件内容
+new_content_file = open(new_content_path, 'r')  #储存已备份文件夹中的内容
+
+old_content = old_content_file.readlines()
+new_content = new_content_file.readlines()
+
+print(new_content)
+print('========================================================================')
+print(old_content)
+# for i in new_content
+#     if i not in old_content
+#         copy_content_file.write(i)
+copy_content_file.close()
